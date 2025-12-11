ac4aa8f70006cb9ba125e3c2fa033992fa6c1aa4
ArtSmile <art.a.kotovich@gmail.com>
2025-12-11 13:39:29 +0200

    Update OriginalTitle.js

--- OriginalTitle.js
+++ OriginalTitle.js
@@ -1,9 +1,8 @@
 (function () {
-  //BDVBurik 2024 (Minimalist Style)
+  //(English Only - Clean Version)
   "use strict";
 
   async function titleOrigin(card) {
-    var orig = card.original_title || card.original_name;
     var params = {
       id: card.id,
       url: "https://worker-patient-dream-26d7.bdvburik.workers.dev:8443/https://api.themoviedb.org/3/movie/",
@@ -22,9 +21,10 @@
       },
     };
 
+    // Оставили только одну функцию - получение английского
     async function getEnTitle() {
       var title;
-      var ftc = await fetch(
+      await fetch(
         params.url + params.id + "?language=en-US" + params.urlEnd,
         getOptions
       )
@@ -33,48 +33,27 @@
 
       return title;
     }
-    async function getRuTitle() {
-      var title;
-      var ftc = await fetch(
-        params.url + params.id + "?language=ru-RU" + params.urlEnd,
-        getOptions
-      )
-        .then((response) => response.json())
-        .then((e) => (title = e.title || e.name));
-      return title;
-    }
 
     var etEnTitle = await getEnTitle();
-    var etRuTitle = await getRuTitle();
     _showEnTitle(etEnTitle);
 
     function _showEnTitle(data) {
-      let ru = "";
       if (data) {
         var render = Lampa.Activity.active().activity.render();
-
-        if (Lampa.Storage.get("language") != "ru") {
-          // Убрал лишние стили у RU, чтобы они наследовались от родителя
-          ru = "<div>Ru: " + etRuTitle + "</div>";
-        } else ru = "";
+        
+        // Убраны все проверки Lampa.Storage и переменная ru
+        
+        $(".original_title", render)
+          .find("> div")
+          .eq(0)
+          .after(
+            `<div id='titleen' style='margin-bottom: 0.6em;'>
+                <div style='font-size: 0.9em; color: #a5a5a5; line-height: 1.2;'>
+                    ${data} 
+                </div>
+             </div>`
+          );
       }
-      
-      $(".original_title", render)
-        .find("> div")
-        .eq(0)
-        .after(
-          // ИЗМЕНЕНИЯ ЗДЕСЬ:
-          // 1. margin-bottom: 0.6em (отступ снизу)
-          // 2. font-size: 0.9em (шрифт меньше)
-          // 3. color: #a5a5a5 (светло-серый цвет)
-          // 4. Убрано "En:" и блок с Orig
-          `<div id='titleen' style='margin-bottom: 0.6em;'>
-              <div style='font-size: 0.9em; color: #a5a5a5; line-height: 1.2;'>
-                  ${data} 
-                  ${ru}
-              </div>
-           </div>`
-        );
     }
   }
 
@@ -84,11 +63,15 @@
       if (e.type == "complite") {
         var render = e.object.activity.render();
         $(".original_title", render).remove();
-        // text-align: left остался, как мы и договаривались
+        
+        // Настройки стиля блока-контейнера
         $(".full-start-new__title", render).after(
           '<div class="original_title" style="margin-top:-0.8em; text-align: left;"><div>'
         );
+        
         titleOrigin(e.data.movie);
+        
+        // Коррекция отступов элементов интерфейса Lampa
         $(".full-start-new__rate-line").css("margin-bottom", "0.8em");
         $(".full-start-new__details").css("margin-bottom", "0.8em");
         $(".full-start-new__tagline").css("margin-bottom", "0.4em");

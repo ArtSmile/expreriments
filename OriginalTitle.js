df038f1ba7a98d8b276a4f7953e46efb88416f30
ArtSmile <art.a.kotovich@gmail.com>
2025-12-11 13:42:21 +0200

    Update OriginalTitle.js

--- OriginalTitle.js
+++ OriginalTitle.js
@@ -1,5 +1,5 @@
 (function () {
-  //(English Only - Clean Version)
+  //BDVBurik 2024 (Final Adjusted Version)
   "use strict";
 
   async function titleOrigin(card) {
@@ -21,7 +21,6 @@
       },
     };
 
-    // Оставили только одну функцию - получение английского
     async function getEnTitle() {
       var title;
       await fetch(
@@ -41,14 +40,16 @@
       if (data) {
         var render = Lampa.Activity.active().activity.render();
         
-        // Убраны все проверки Lampa.Storage и переменная ru
-        
         $(".original_title", render)
           .find("> div")
           .eq(0)
           .after(
-            `<div id='titleen' style='margin-bottom: 0.6em;'>
-                <div style='font-size: 0.9em; color: #a5a5a5; line-height: 1.2;'>
+            // ИЗМЕНЕНИЯ ЗДЕСЬ:
+            // margin-top: 0.3em (половина от нижнего)
+            // margin-bottom: 0.6em
+            // font-size: 1.1em (средний размер)
+            `<div id='titleen' style='margin-bottom: 0.6em; margin-top: 0.3em;'>
+                <div style='font-size: 1.1em; color: #a5a5a5; line-height: 1.2;'>
                     ${data} 
                 </div>
              </div>`
@@ -64,14 +65,12 @@
         var render = e.object.activity.render();
         $(".original_title", render).remove();
         
-        // Настройки стиля блока-контейнера
         $(".full-start-new__title", render).after(
           '<div class="original_title" style="margin-top:-0.8em; text-align: left;"><div>'
         );
         
         titleOrigin(e.data.movie);
         
-        // Коррекция отступов элементов интерфейса Lampa
         $(".full-start-new__rate-line").css("margin-bottom", "0.8em");
         $(".full-start-new__details").css("margin-bottom", "0.8em");
         $(".full-start-new__tagline").css("margin-bottom", "0.4em");

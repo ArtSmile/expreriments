00be03e4f16fcfdf0f5a6aab110af786b27a9ed0
ArtSmile <art.a.kotovich@gmail.com>
2026-01-15 09:27:50 +0200

    Update CUB_Dashboard_Settings.js

--- CUB_Dashboard_Settings.js
+++ CUB_Dashboard_Settings.js
@@ -1,4 +1,3 @@
-
 (function () {
     'use strict';
 
@@ -34,11 +33,7 @@
                 icon: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10 0h8V11h-8v10zm0-18v6h8V3h-8z" fill="white"/></svg>'
             });
 
-            Lampa.Settings.listener.follow('open', function (e) {
-                if (e.name == 'cub_dashboard') {
-                    _this.renderSettings();
-                }
-            });
+            this.renderSettings();
 
 
             this.interceptContentRows();
